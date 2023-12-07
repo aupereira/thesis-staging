447533d8b6c809fbcc8c1ebe7869bddaf13eddcf
Aurora Pereira <aupereira@umass.edu>
2023-12-07 00:14:18 -0500

    Merged Radix-2 FFT

--- README.md
+++ README.md
@@ -1,3 +1,25 @@
-# ThesisTesting
+<h2> Staging area for 499Y/T </h2>
 
-<p>This is a repo to keep *work in progress* versions of a test suite for my thesis.</p>
+This repo contains work in progress versions of the benchmarks used for my thesis. Some things are broken, some things are incomplete, and some things work just fine but will probably be revised in the later stages.
+
+<h3> Current Benchmarks </h3>
+1. SHA256
+    * Currently functional - produces correct hashes on very large files (tested up to 28 GB)
+    * Memory management needs revision (lots of time in malloc)
+    * Needs large file (it's not that expensive to compute) - planned ~30-50 GB
+    * May switch to a two-threaded model with double buffering to limit time waiting to load files
+2. FFT
+    * Utilizes a Radix-2 DIT FFT algorithm
+    * Almost works correctly
+    * Rust has some odd performance behaviors when choosing between Vec and Array types. Need to investigate that further.
+    * Rust also gets handily beat by C# in this test.
+3. Eigenvalue
+    * Probably going to use Householder reflectors and QR decomposition
+    * I'm working on it (I've spent quite a few hours learning linalg for this)
+4. DataTransform
+    * Planned features (though highly dependent on how intensive it is)
+    * Some sorting algorithms, including sorting by string
+    * 
+5. Neural Net
+    * Probably an MLP
+    * Likely will borrow from prior dataset.
--- src/fft/fft_rs/src/main.rs
+++ src/fft/fft_rs/src/main.rs
@@ -1,66 +1,213 @@
-use num_cpus;
 use num::complex::Complex;
-use std::ops::{Add, Sub, Mul};
 use std::f64::consts::PI;
 use rand::Rng;
 use std::thread;
+use std::env;
 
-pub struct Complex {
-    pub re: T,
-    pub im: T,
+fn rng_complex() -> Complex<f64> {
+    let mut rng = rand::thread_rng();
+    let complex = num::complex::Complex::new(
+        rng.gen_range(-1.0..1.0),
+        rng.gen_range(-1.0..1.0)
+    );
+    return complex;
 }
 
-impl Mul for Complex {
-    type Output = Complex;
+fn fft(x: &mut [Complex<f64>]) {
+    let n: usize = x.len();
 
-    fn mul(self, other: Complex) -> Complex {
-        Complex {
-            re: self.re * other.re - self.im * other.im,
-            im: self.re * other.im + self.im * other.re,
-        }
-    }
-}
-
-fn fft(x: &mut [f64]) -> Vec<Complex> {
-    let N = x.len();
-
-    let halfN = N / 2;
-    
-    if N <= 1 {
-        return x;
+    if n <= 1 {
+        return;
     }
 
-    let even = fft(x[0..N:2]);
-    let odd = fft(x[1..N:2]);
+    let mut even: Vec<Complex<f64>> = Vec::new();
+    let mut odd: Vec<Complex<f64>> = Vec::new();
 
-    for k in 0..halfN {
-        let t = Complex::from_polar(&1.0, (&-2.0 * PI * k as f64 / N as f64)) * odd[k];
+    for i in 0..n / 2 {
+        even.push(x[2 * i]);
+        odd.push(x[2 * i + 1]);
+    }
+
+    fft(&mut even);
+    fft(&mut odd);
+
+    for k in 0..n / 2 {
+        let t = Complex::exp(-2.0 * PI * k as f64 / n as f64 * Complex::i()) * odd[k];
         x[k] = even[k] + t;
-        x[k + halfN] = odd[k] - t;
+        x[k + n / 2] = even[k] - t;
+    }
+}
+
+fn fft_loop(size: usize, loops: usize) {
+    let mut x: Vec<Complex<f64>>;
+    
+    for _i in 0..loops {
+        x = (0..size).map(|_| rng_complex()).collect();
+        fft(&mut x);
     }
 
-    return x;
+    return;
 }
 
 fn main() {
-    // let num_threads = num_cpus::get();
+    let args: Vec<String> = env::args().collect();
 
-    // let handles: Vec<_> = (0..num_threads)
-    //     .map(|_| {
-    //         thread::spawn(|| {
-    //             println!("Hello World!");
-    //         })
-    //     })
-    //     .collect();
+    if args.len() != 4 {
+        println!("Usage: ./fft <fft_size> <num_loops> <num_threads>");
+        return;
+    }
 
-    // for handle in handles {
-    //     handle.join().unwrap();
-    // }
+    let fft_size: usize = 2usize.pow(args[1].parse::<u32>().unwrap());
+    let num_loops: usize = args[2].parse::<usize>().unwrap();
+    let num_threads: usize = args[3].parse::<usize>().unwrap();
 
-    let mut x: Vec<f64> = (0..64).map(|_| rng.gen()).collect();
-    println!(fft(&mut x))
+    let handles: Vec<_> = (0..num_threads)
+        .map(|_| {
+            let fs: usize = fft_size.clone();
+            let nl: usize = num_loops.clone();
+            thread::spawn(move || {
+                fft_loop(fs, nl);
+            })
+        })
+        .collect();
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    // let fft_size: u64 = 65536;
+    // let num_loops: u64 = 10;
+    // let num_threads: u64 = 12;
+
+    // let a: Complex<f64> = Complex::new(1.0, 2.0);
+    // let b: Complex<f64> = Complex::new(3.0, -4.0);
+    // let c: Complex<f64> = Complex::new(5.0, 6.0);
+    // let d: Complex<f64> = Complex::new(7.0, -8.0);
+    
+    // let mut x: Vec<Complex<f64>> = [a, b, c, d].to_vec();
+
+    // fft(&mut x);
+
+    // println!("{:?}", x);
 }
 
-//     if (ceil(log2(N)) != floor(log2(N))){
-//     panic!("N is not a power of 2");
+
+// Might want to test this for speed.
+
+// use num_cpus;
+// use std::ops::{Add, Sub, Mul};
+
+// struct Complex {
+//     re: f64,
+//     im: f64,
+// }
+
+// impl Add for Complex {
+//     type Output = Complex;
+
+//     fn add(self, other: Complex) -> Complex {
+//         Complex {
+//             re: self.re + other.re,
+//             im: self.im + other.im,
+//         }
+//     }
+// }
+
+// impl Sub for Complex {
+//     type Output = Complex;
+
+//     fn sub(self, other: Complex) -> Complex {
+//         Complex {
+//             re: self.re - other.re,
+//             im: self.im - other.im,
+//         }
+//     }
+// }
+
+// impl Mul for Complex {
+//     type Output = Complex;
+
+//     fn mul(self, other: Complex) -> Complex {
+//         Complex {
+//             re: self.re * other.re - self.im * other.im,
+//             im: self.re * other.im + self.im * other.re,
+//         }
+//     }
+// }
+
+// impl Complex {
+//     fn exp(self) -> Complex {
+//         Complex {
+//             re: self.re.exp() * self.im.cos(),
+//             im: self.re.exp() * self.im.sin(),
+//         }
+//     }
+// }
+
+// fn rng_complex() -> Complex {
+//     let mut rng = rand::thread_rng();
+//     Complex {
+//         re: rng.gen_range(-1.0..1.0),
+//         im: rng.gen_range(-1.0..1.0),
+//     }
+// }
+
+
+
+// fn fft_failed_attempt_1(x: &mut [Complex<f64>]) {
+//     let n: usize = x.len();
+
+//     let half_n: usize = n / 2;
+    
+//     if n <= 1 {
+//         return;
+//     }
+
+//     let mut even: Vec<Complex<f64>> = Vec::new();
+//     let mut odd: Vec<Complex<f64>> = Vec::new();
+
+//     for i in 0..half_n {
+//         even.push(x[2*i]);
+//         odd.push(x[2*i + 1]);
+//     }
+
+//     //let (even, odd) = x.split_at_mut(half_n);
+    
+//     fft(&mut even);
+//     fft(&mut odd);
+
+//     for k in 0..half_n {
+//         let t = Complex::exp(Complex::new(-2.0 * PI * k as f64 / n as f64, 0.0)) * odd[k];
+//         x[k] = even[k] + t;
+//         x[k + half_n] = odd[k] - t;
+//     }
+
+//     return;
+// }
+
+// fn fft_failed_attempt_2(x: &[Complex<f64>]) -> Vec<Complex<f64>> {
+//     let n: usize = x.len();
+
+//     let half_n: usize = n / 2;
+    
+//     if n <= 1 {
+//         return x.to_vec();
+//     }
+
+//     let mut even: Vec<_> = x.iter().enumerate().filter_map(|(i, v)| if i % 2 == 0 { Some(*v) } else { None }).collect();
+//     let mut odd: Vec<_> = x.iter().enumerate().filter_map(|(i, v)| if i % 2 != 0 { Some(*v) } else { None }).collect();
+//     even = fft(&even);
+//     odd = fft(&odd);
+
+//     let mut out: Vec<Complex<f64>> = vec![Complex::new(0.0, 0.0); n];
+
+//     let mut t: Complex<f64>;
+
+//     for k in 0..half_n {
+//         t = Complex::exp(-2.0 * PI * k as f64 / n as f64 * Complex::i()) * odd[k];
+//         out[k] = even[k] + t;
+//         out[k + half_n] = odd[k] - t;
+//     }
+
+//     return out;
 // }
