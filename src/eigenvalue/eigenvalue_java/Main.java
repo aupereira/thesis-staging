c5aa4cea5c4f07754cee0deb3300184df63d15ac
Aurora Pereira <aupereira@umass.edu>
2024-01-15 11:41:43 -0500

    Eigenvalue Java multithreading and optimizations

--- src/eigenvalue/eigenvalue_java/Main.java
+++ src/eigenvalue/eigenvalue_java/Main.java
@@ -1,6 +1,14 @@
-package eigenvalue_java;
+// package eigenvalue_java;
 
 class Main {
+    static void printVec(double[] v) {
+        System.out.println();
+        for (int i = 0; i < v.length; i++) {
+            System.out.printf("%f ", v[i]);
+        }
+        System.out.println();
+    }
+    
     static void printMat(double[][] A) {
         System.out.println();
         for (int i = 0; i < A.length; i++) {
@@ -12,341 +20,244 @@ class Main {
         }
         System.out.println();
     }
-    
-    static double[][] rndSymmetric(int n) {
-        double[][] A = new double[n][n];
 
-        for (int i = 0; i < n; i++) {
+    static void mCopy(int len, double[][] FROM, double[][] TO) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                TO[i][j] = FROM[i][j];
+            }
+        }
+    }
+
+    static void rndSymmetric(double[][] A) {
+        int len = A.length;
+
+        for (int i = 0; i < len; i++) {
             A[i][i] = Math.random();
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < len; j++) {
                 A[i][j] = Math.random();
                 A[j][i] = A[i][j];
             }
         }
-        
-        return A;
-    }
-    
-    static double[][] identity(int n) {
-        double[][] I = new double[n][n];
-        for (int i = 0; i < n; i++) {
-            I[i][i] = 1;
-        }
-        return I;
     }
 
-    static double DNRM2(double[] A) {
+    static double sgn(double x) {
+        return x >= 0.0 ? 1.0 : -1.0;
+    }
+
+    static double norm2(int len, double[] v) {
         double norm = 0;
-        for (int i = 0; i < A.length; i++) {
-            norm += A[i] * A[i];
+        for (int i = 0; i < len; i++) {
+            norm += v[i] * v[i];
         }
         return Math.sqrt(norm);
     }
 
-    static double[] stdBasis(int len, int pos) {
-        double[] e = new double[len];
-        e[pos] = 1;
-        return e;
-    }
-    
-    static double[][] mul2OuterProd(double[] v) {
-        int I = v.length;
-        double[][] A = new double[I][I];
-        
-        for (int i = 0; i < I; i++) {
-            for (int j = 0; j < I; j++) {
-                A[i][j] = -2.0 * v[i] * v[j];
-            }
-        }
-        
-        return A;
-    }
-    
-    static double[] vAdd(double[] v1, double[] v2) {
-        double[] w = new double[v1.length];
-        for (int i = 0; i < v1.length; i++) {
-            w[i] = v1[i] + v2[i];
-        }
-        return w;
-    }
-
-    static double[] vMulS(double[] v, double s) {
-        double[] w = new double[v.length];
-        for (int i = 0; i < v.length; i++) {
-            w[i] = v[i] * s;
-        }
-        return w;
-    }
-
-    static void vDivS(double[] v, double s) {
-        for (int i = 0; i < v.length; i++) {
+    static void vDivS(int len, double[] v, double s) {
+        for (int i = 0; i < len; i++) {
             v[i] /= s;
         }
     }
 
-    static double[][] mAdd(double[][] A, double[][] B) {
-        int I = A.length;
-        int J = A[0].length;
-        
-        for (int i = 0; i < I; i++) {
-            for (int j = 0; j < J; j++) {
-                A[i][j] += B[i][j];
+    static void S2OPAI(int len, double[] v, double[][] A) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                A[i][j] = -2.0 * v[i] * v[j];
             }
         }
 
-        return A;
+        for (int i = 0; i < len; i++) {
+            A[i][i] += 1.0;
+        }
     }
 
-    static double[][] mSub(double[][] A, double[][] B) {
-        int I = A.length;
-        int J = A[0].length;
-        
-        for (int i = 0; i < I; i++) {
-            for (int j = 0; j < J; j++) {
-                A[i][j] -= B[i][j];
+    static void matrixMul(int len, double[][] A, double[][] B, double[][] C, double[][] BTMP) {
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                BTMP[i][j] = B[j][i];
             }
         }
 
-        return A;
-    }
-    
-    static double sgn(double x) {
-        if (x >= 0.0) {
-            return 1.0;
-        }
-        else {
-            return -1.0;
-        }
-    }
-
-    static double[][] transpose(double[][] A) {
-        int n = A.length;
-        int m = A[0].length;
-
-        double[][] B = new double[m][n];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                B[j][i] = A[i][j];
-            }
-        }
-
-        return B;
-    }
-
-    static void transposeIP(double[][] A) {
-        int n = A.length;
-        int m = A[0].length;
-        
-        double temp;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < m; j++) {
-                temp = A[i][j];
-                A[i][j] = A[j][i];
-                A[j][i] = temp;
-            }
-        }
-
-        return;
-    }
-
-    static double[][] GEMM(double[][] A, double[][] B) {
-        double t_B[][] = transpose(B);
-        
-        int I = A.length;
-        int J = B.length;
-        int K = A[0].length;
-
-        double[][] C = new double[I][J];
-
-        for (int i = 0; i < I; i++) {
-            for (int j = 0; j < J; j++) {
-                for (int k = 0; k < K; k++) {
-                    C[i][j] += A[i][k] * t_B[j][k];
+        for (int i = 0; i < len; i++) {
+            for (int j = 0; j < len; j++) {
+                C[i][j] = 0;
+                for (int k = 0; k < len; k++) {
+                    C[i][j] += A[i][k] * BTMP[j][k];
                 }
             }
         }
-
-        return C;
     }
 
-    static double[][] tridiagonalize(double[][] A) {
-        int n = A.length;
-        int m = A[0].length;
-
+    static void tridiagonalize(int N, double[][] A, double[][] P, double[][] TEMP, double[][] TPOSE, double[] v) {
         double alpha;
         double r;
-        double[] v;
 
-        double[][] P = new double[n][n];
-
-        for (int k = 0; k < m - 2; k++) {
-            v = new double[n];
-            for (int j = k + 1; j < n; j++) {
+        for (int k = 0; k < N - 2; k++) {
+            for (int j = 0; j < k + 1; j++) {
+                v[j] = 0.0;
+            }
+            for (int j = k + 1; j < N; j++) {
                 v[j] = A[j][k];
             }
-            
-            alpha = -sgn(v[k + 1]) * DNRM2(v);
+        
+            alpha = -sgn(v[k + 1]) * norm2(N, v);
             r = 2.0 * Math.sqrt(0.5 * ((alpha * alpha) - (alpha * v[k + 1])));
 
-            v[k + 1] = (v[k + 1] - alpha) / (r);
-            for (int j = k + 2; j < n; j++) {
-                v[j] = v[j] / r; 
+            v[k + 1] = (v[k + 1] - alpha) / r;
+            for (int j = k + 2; j < N; j++) {
+                v[j] /= r;
             }
 
-            P = mAdd(identity(n), mul2OuterProd(v));
-            A = GEMM(GEMM(P, A), P);
+            S2OPAI(N, v, P);
+            matrixMul(N, P, A, TEMP, TPOSE);
+            matrixMul(N, TEMP, P, A, TPOSE);
         }
-
-        return A;
     }
 
-    static double[][] householder_no_shift(double[][] R) {
-        int n = R.length;
-        int m = R[0].length;
+    static void householder(int len, double[][] A, double[][] A1, double[][] Q, double[][] H, double[][] TEMP, double[][] TPOSE, double[] v) {
+        mCopy(len, A, A1);
+        double[][] SWAP;
 
-        double[][] Q = new double[n][n];
-        double[][] H;
-        double[] v;
+        double s = A1[len - 1][len - 1];
+        double t = A1[len - 2][len - 2];
+        double x = A1[len - 2][len - 1];
+        double d = (t - s) / 2.0;
+        double shift = s - x * x / (d + sgn(d) * Math.sqrt(d * d + x * x));
 
-        for (int k = 0; k < m - 1; k++) {
-            v = new double[n];
-            for (int j = k; j < n; j++) {
-                v[j] = R[j][k];
+        for (int i = 0; i < len; i++) {
+            A1[i][i] -= shift;
+        }
+
+        for (int k = 0; k < len - 1; k++) {
+            for (int j = 0; j < k; j++) {
+                v[j] = 0.0;
+            }
+            for (int j = k; j < len; j++) {
+                v[j] = A1[j][k];
             }
 
-            v = vAdd(v, vMulS(stdBasis(n, k), DNRM2(v) * sgn(v[k + 1])));
-            vDivS(v, DNRM2(v));
-            H = mAdd(identity(n), mul2OuterProd(v));
-            R = GEMM(H, R);
+            v[k] += norm2(len, v) * sgn(v[k + 1]);
+            vDivS(len, v, norm2(len, v));
+
             if (k == 0) {
-                Q = H;
-            } else {
-                Q = GEMM(Q, H);
+                S2OPAI(len, v, Q);
+                matrixMul(len, Q, A1, TEMP, TPOSE);
+                SWAP = A1;
+                A1 = TEMP;
+                TEMP = SWAP;
             }
-        }
-        
-        return GEMM(R, Q);
-    }
-
-    static double[][] householder(double[][] A) {
-        int n = A.length;
-        int m = A[0].length;
-
-        double[][] Q = new double[n][n];
-        double[][] H;
-        double[] v;
-
-        double s = A[n - 1][n - 1];
-        double t = A[n - 2][n - 2];
-        double x = A[n - 2][n - 1];
-        double d = (t - s) / 2;
-        double shift = s - x * x / (d + (d >= 0 ? 1 : -1) * Math.sqrt(d * d + x * x));
-
-        for (int i = 0; i < n; i++) {
-            A[i][i] -= shift;
-        }
-
-        for (int k = 0; k < m - 1; k++) {
-            v = new double[n];
-            for (int j = k; j < n; j++) {
-                v[j] = A[j][k];
-            }
-
-            v = vAdd(v, vMulS(stdBasis(n, k), DNRM2(v) * sgn(v[k + 1])));
-            vDivS(v, DNRM2(v));
-            H = mAdd(identity(n), mul2OuterProd(v));
-            A = GEMM(H, A);
-            if (k == 0) {
-                Q = H;
-            } else {
-                Q = GEMM(Q, H);
+            else {
+                S2OPAI(len, v, H);
+                matrixMul(len, H, A1, TEMP, TPOSE);
+                SWAP = A1;
+                A1 = TEMP;
+                TEMP = SWAP;
+                matrixMul(len, Q, H, TEMP, TPOSE);
+                SWAP = Q;
+                Q = TEMP;
+                TEMP = SWAP;
             }
         }
 
-        A = GEMM(A, Q);
+        matrixMul(len, A1, Q, A, TPOSE);
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < len; i++) {
             A[i][i] += shift;
         }
-        
-        return A;
     }
 
-    static double[] eigQR(double[][] A) {
-        double[] vals = new double[A.length];
+    static void eigQR(double[][] A, double[] eigVals, double[][] T1, double[][] T2, double[][] T3, double[][] T4, double[][] T5, double[] v) {
+        int N = A.length;
 
-        double currentEigenvalue = A[A.length - 1][A[0].length - 1];
-        double prevEigenvalue = Double.MAX_VALUE;
+        double currentEigval;
+        double prevEigval = Double.MAX_VALUE;
 
-        int ind = A.length - 1;
+        int ind = N;
 
-        while (A.length > 1) {
+        while (ind > 1) {
             boolean flag = true;
             while (flag) {
                 // System.out.printf("%d\n", ind); // Useful for QR input size tuning
-                A = householder(A);
+                householder(ind, A, T1, T2, T3, T4, T5, v);
                 
-                currentEigenvalue = A[A.length - 1][A[0].length - 1];
+                currentEigval = A[ind - 1][ind - 1];
         
-                if (Math.abs(currentEigenvalue - prevEigenvalue) < 1e-10) {
-                    vals[ind] = currentEigenvalue;
+                if (Math.abs(currentEigval - prevEigval) < 1e-10) {
+                    eigVals[ind - 1] = currentEigval;
                     
                     flag = false;
                     ind--;
                     
-                    double[][] temp = new double[A.length - 1][A[0].length - 1];
-                    for (int i = 0; i < temp.length; i++) {
-                        for (int j = 0; j < temp[0].length; j++) {
-                            temp[i][j] = A[i][j];
-                        }
-                    }
-                    A = temp;
-                    
-                    prevEigenvalue = A[A.length - 1][A[0].length - 1];
+                    prevEigval = A[ind - 1][ind - 1];
                 } else {
-                    prevEigenvalue = currentEigenvalue;
+                    prevEigval = currentEigval;
                 }
             }
+            eigVals[0] = A[0][0];
         }
+    }
 
-        vals[0] = A[0][0];
+    static void eigenLoop(int size, int loops) {
+        double[][] A = new double[size][size];
 
-        return vals;
+        double[][] T1 = new double[size][size];
+        double[][] T2 = new double[size][size];
+        double[][] T3 = new double[size][size];
+        double[][] T4 = new double[size][size];
+        double[][] T5 = new double[size][size];
+        double[] v = new double[size];
+
+        double[] eigvals = new double[size];
+
+        for (int i = 0; i < loops; i++) {
+            rndSymmetric(A);
+            tridiagonalize(size, A, T1, T2, T3, v);
+            eigQR(A, eigvals, T1, T2, T3, T4, T5, v);
+        }
     }
 
     public static void main(String[] args) {
-        int iter;
-        int size;
+        if (args.length != 3) {
+            System.out.println("Usage: java Main <matrix size> <num loops> <num threads>");
+        }
 
-        // if (args.length == 2) {
-        //     iter = Integer.parseInt(args[0]);
-        //     size = Integer.parseInt(args[1]);
-        //     System.out.println("Running " + iter + " iterations of size " + size);
-        // } else {
-        //     iter = 1;
-        //     size = 96;
-        //     System.out.println("To specify iterations and size, run:\n    java Main <iterations> <size>");
-        //     System.out.println("Proceeding with default values: %d iterations of size %d".formatted(iter, size));
-        // }
+        int mSize = Integer.parseInt(args[0]);
+        int numLoops = Integer.parseInt(args[1]);
+        int numThreads = Integer.parseInt(args[2]);
 
-        // for (int i = 0; i < iter; i++) {
-        //     eigQR(tridiagonalize(rndSymmetric(size)));
-        //     System.out.println(i);
-        // }
+        Thread[] threads = new Thread[numThreads];
 
-        // Basic validation test for tridiagonalization algorithm
-        // double[][] test = {{4.0, 1.0, -2.0, 2.0}, {1.0, 2.0, 0.0, 1.0}, {-2.0, 0.0, 3.0, -2.0}, {2.0, 1.0, -2.0, -1.0}};
-        // double[][] test = rndSymmetric(100);
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new Thread(() -> eigenLoop(mSize, numLoops));
+        }
 
-        // test = tridiagonalize(test);
-        // printMat(tridiagonalize(test));
-        
-        // double[] eig = eigQR(test);
+        for (int i = 0; i < numThreads; i++) {
+            threads[i].start();
+        }
 
-        // for (int i = 0; i < eig.length; i++) {
-        //     System.out.printf("%f ", eig[i]);
-        // }
+        for (int i = 0; i < numThreads; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+        }
+
+        // int mSize = 4;
+        // double[][] A = {{4.0, 1.0, -2.0, 2.0}, {1.0, 2.0, 0.0, 1.0}, {-2.0, 0.0, 3.0, -2.0}, {2.0, 1.0, -2.0, -1.0}};
+
+        // double[][] T1 = new double[mSize][mSize];
+        // double[][] T2 = new double[mSize][mSize];
+        // double[][] T3 = new double[mSize][mSize];
+        // double[][] T4 = new double[mSize][mSize];
+        // double[][] T5 = new double[mSize][mSize];
+        // double[] v = new double[mSize];
+
+        // double[] eigvals = new double[mSize];
+
+        // tridiagonalize(mSize, A, T1, T2, T3, v);
+        // eigQR(A, eigvals, T1, T2, T3, T4, T5, v);
+
+        // printVec(eigvals);
     }
 }
