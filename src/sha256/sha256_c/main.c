2c2d72b08b83dd3965a3e0db81c23926c5e284a2
Aurora Pereira <aupereira@umass.edu>
2024-01-22 22:58:53 -0500

    Changed SHA256 to SHA512

--- src/sha256/sha256_c/main.c
+++ src/sha256/sha256_c/main.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
+
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
 #define NODE_SIZE 4096
 
 union buf
 {
-    uint8_t bytes[64];
-    uint32_t dwords[16];
-    uint64_t qwords[8];
+    uint8_t bytes[128];
+    uint64_t qwords[16];
 };
 
 struct bufNode
@@ -20,109 +20,74 @@ struct bufNode
     struct bufNode *next;
 };
 
-enum bufferStatus {Continue, AddPadBlock, Done};
-
-bool littleEndian;
-struct bufNode *head, *current;
-uint64_t totalNodes, bufferIndex, totalBits;
-
-uint32_t K[] =
+enum bufferStatus
 {
-    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 
-    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 
-    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 
-    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+    Continue,
+    AddPadBlock,
+    Done
 };
 
-bool isLittleEndian()
-{
-    uint32_t n = 0x00000001;
-    uint8_t *p = (uint8_t *)&n;
-    return (*p == 0x01);
-}
+struct bufNode *head, *current;
+uint64_t totalNodes, bufferIndex;
+__uint128_t totalBits;
 
-uint32_t chgEndUInt32(uint32_t n)
-{
-    uint32_t r = n >> 24 | (n & 0x00FF0000) >> 8 | (n & 0x0000FF00) << 8 | n << 24;
-    
-    return r;
-}
+uint64_t K[] =
+    {
+        0x428a2f98d728ae22, 0x7137449123ef65cd, 0xb5c0fbcfec4d3b2f, 0xe9b5dba58189dbbc,
+        0x3956c25bf348b538, 0x59f111f1b605d019, 0x923f82a4af194f9b, 0xab1c5ed5da6d8118,
+        0xd807aa98a3030242, 0x12835b0145706fbe, 0x243185be4ee4b28c, 0x550c7dc3d5ffb4e2,
+        0x72be5d74f27b896f, 0x80deb1fe3b1696b1, 0x9bdc06a725c71235, 0xc19bf174cf692694,
+        0xe49b69c19ef14ad2, 0xefbe4786384f25e3, 0x0fc19dc68b8cd5b5, 0x240ca1cc77ac9c65,
+        0x2de92c6f592b0275, 0x4a7484aa6ea6e483, 0x5cb0a9dcbd41fbd4, 0x76f988da831153b5,
+        0x983e5152ee66dfab, 0xa831c66d2db43210, 0xb00327c898fb213f, 0xbf597fc7beef0ee4,
+        0xc6e00bf33da88fc2, 0xd5a79147930aa725, 0x06ca6351e003826f, 0x142929670a0e6e70,
+        0x27b70a8546d22ffc, 0x2e1b21385c26c926, 0x4d2c6dfc5ac42aed, 0x53380d139d95b3df,
+        0x650a73548baf63de, 0x766a0abb3c77b2a8, 0x81c2c92e47edaee6, 0x92722c851482353b,
+        0xa2bfe8a14cf10364, 0xa81a664bbc423001, 0xc24b8b70d0f89791, 0xc76c51a30654be30,
+        0xd192e819d6ef5218, 0xd69906245565a910, 0xf40e35855771202a, 0x106aa07032bbd1b8,
+        0x19a4c116b8d2d0c8, 0x1e376c085141ab53, 0x2748774cdf8eeb99, 0x34b0bcb5e19b48a8,
+        0x391c0cb3c5c95a63, 0x4ed8aa4ae3418acb, 0x5b9cca4f7763e373, 0x682e6ff3d6b2b8a3,
+        0x748f82ee5defb2fc, 0x78a5636f43172f60, 0x84c87814a1f0ab72, 0x8cc702081a6439ec,
+        0x90befffa23631e28, 0xa4506cebde82bde9, 0xbef9a3f7b2c67915, 0xc67178f2e372532b,
+        0xca273eceea26619c, 0xd186b8c721c0c207, 0xeada7dd6cde0eb1e, 0xf57d4f7fee6ed178,
+        0x06f067aa72176fba, 0x0a637dc5a2c898a6, 0x113f9804bef90dae, 0x1b710b35131c471b,
+        0x28db77f523047d84, 0x32caab7b40c72493, 0x3c9ebe0a15c9bebc, 0x431d67c49c100d4c,
+        0x4cc5d4becb3e42b6, 0x597f299cfc657e2a, 0x5fcb6fab3ad6faec, 0x6c44198c4a475817
+    };
 
+#define rotr(x, n) ((x >> n) | (x << (64 - n)))
+
+#define ch(x, y, z) ((x & y) ^ (~x & z))
+
+#define maj(x, y, z) ((x & y) ^ (x & z) ^ (y & z))
+
+#define Sig0(x) (rotr(x, 28) ^ rotr(x, 34) ^ rotr(x, 39))
+
+#define Sig1(x) (rotr(x, 14) ^ rotr(x, 18) ^ rotr(x, 41))
+
+#define sig0(x) (rotr(x, 1) ^ rotr(x, 8) ^ (x >> 7))
+
+#define sig1(x) (rotr(x, 19) ^ rotr(x, 61) ^ (x >> 6))
+
+#ifdef __x86_64__
 uint64_t chgEndUInt64(uint64_t n)
 {
-    uint64_t r = 
+    uint64_t r =
         (n >> 56) | (n >> 40) & 0x000000000000FF00 |
-	    (n >> 24) & 0x0000000000FF0000 | (n >> 8) & 0x00000000FF000000 |
-	    (n << 8) & 0x000000FF00000000 | (n << 24) & 0x0000FF0000000000 |
-	    (n << 40) & 0x00FF000000000000 | (n << 56)
-    ;
+        (n >> 24) & 0x0000000000FF0000 | (n >> 8) & 0x00000000FF000000 |
+        (n << 8) & 0x000000FF00000000 | (n << 24) & 0x0000FF0000000000 |
+        (n << 40) & 0x00FF000000000000 | (n << 56);
 
     return r;
 }
-
-uint32_t rotl(uint32_t x, uint8_t n)
-{
-    return (x << n) | (x >> (32 - n));
-}
-
-uint32_t rotr(uint32_t x, uint8_t n)
-{
-    return (x >> n) | (x << (32 - n));
-}
-
-uint32_t shr(uint32_t x, uint8_t n)
-{
-    return x >> n;
-}
-
-uint32_t ch(uint32_t x, uint32_t y, uint32_t z)
-{
-    return (x & y) ^ (~x & z);
-}
-
-uint32_t maj(uint32_t x, uint32_t y, uint32_t z)
-{
-    return (x & y) ^ (x & z) ^ (y & z);
-}
-
-uint32_t Sig0(uint32_t x)
-{
-    return rotr(x, 2) ^ rotr(x, 13) ^ rotr(x, 22);
-}
-
-uint32_t Sig1(uint32_t x)
-{
-    return rotr(x, 6) ^ rotr(x, 11) ^ rotr(x, 25);
-}
-
-uint32_t sig0(uint32_t x)
-{
-    return rotr(x, 7) ^ rotr(x, 18) ^ shr(x, 3);
-}
-
-uint32_t sig1(uint32_t x)
-{
-    return rotr(x, 17) ^ rotr(x, 19) ^ shr(x, 10);
-}
+#endif
 
 void createBuffers()
 {
     head = (struct bufNode *)malloc(sizeof(struct bufNode));
     current = head;
 
-    if(head == NULL)
+    if (head == NULL)
     {
         printf("Unable to allocate memory.");
         exit(0);
@@ -138,7 +103,7 @@ void addBuffers()
     struct bufNode *next;
     next = (struct bufNode *)malloc(sizeof(struct bufNode));
 
-    if(next == NULL)
+    if (next == NULL)
     {
         printf("Unable to allocate memory.");
         exit(0);
@@ -160,9 +125,9 @@ void buffGen(char filePath[])
     int byteIndex;
 
     createBuffers();
-    
-    fp = fopen(filePath,"rb+");
-    if(fp == NULL)
+
+    fp = fopen(filePath, "rb+");
+    if (fp == NULL)
     {
         printf("Error opening file.");
         bs = Done;
@@ -175,7 +140,7 @@ void buffGen(char filePath[])
 
     while (bs == Continue)
     {
-        current->buffer[bufferIndex] = (union buf*)malloc(sizeof(union buf));
+        current->buffer[bufferIndex] = (union buf *)malloc(sizeof(union buf));
 
         if (current->buffer[bufferIndex] == NULL)
         {
@@ -183,35 +148,36 @@ void buffGen(char filePath[])
             exit(0);
         }
 
-        byteIndex = fread(current->buffer[bufferIndex]->bytes, 1, 64, fp);
+        byteIndex = fread(current->buffer[bufferIndex]->bytes, 1, 128, fp);
         totalBits += byteIndex * 8;
 
-        if (byteIndex < 56)
+        if (byteIndex < 112)
         {
             current->buffer[bufferIndex]->bytes[byteIndex] = 0x80;
             byteIndex += 1;
-            while (byteIndex < 56)
+            while (byteIndex < 112)
             {
                 current->buffer[bufferIndex]->bytes[byteIndex] = 0x00;
                 byteIndex += 1;
             }
-            switch (littleEndian)
-            {
-                case true:
-                    current->buffer[bufferIndex]->qwords[7] = chgEndUInt64(totalBits);
-                    break;
-                default: 
-                    current->buffer[bufferIndex]->qwords[7] = totalBits;
-                    break;
-            }
-            byteIndex += 8;
+
+#ifdef __x86_64__
+            current->buffer[bufferIndex]->qwords[14] = chgEndUInt64((uint64_t)(totalBits >> 64));
+            current->buffer[bufferIndex]->qwords[15] = chgEndUInt64((uint64_t)(totalBits));
+#endif
+#ifdef _M_ARM64
+            current->buffer[bufferIndex]->qwords[14] = (uint64_t)(totalBits >> 64);
+            current->buffer[bufferIndex]->qwords[15] = (uint64_t)(totalBits);
+#endif
+
+            byteIndex += 16;
             bs = Done;
         }
-        else if (byteIndex < 64)
+        else if (byteIndex < 128)
         {
             current->buffer[bufferIndex]->bytes[byteIndex] = 0x80;
             byteIndex += 1;
-            while (byteIndex < 64)
+            while (byteIndex < 128)
             {
                 current->buffer[bufferIndex]->bytes[byteIndex] = 0x00;
                 byteIndex += 1;
@@ -227,28 +193,29 @@ void buffGen(char filePath[])
         {
             bufferIndex += 1;
         }
-        
+
         if (bufferIndex == NODE_SIZE)
         {
             addBuffers();
         }
     }
 
-    if (bs == AddPadBlock){
-        current->buffer[bufferIndex] = (union buf*)malloc(sizeof(union buf));
-        for (int i = 0; i < 7; i++)
+    if (bs == AddPadBlock)
+    {
+        current->buffer[bufferIndex] = (union buf *)malloc(sizeof(union buf));
+        for (int i = 0; i < 14; i++)
         {
             current->buffer[bufferIndex]->qwords[i] = 0x0000000000000000;
         }
-        switch (littleEndian)
-        {
-            case true:
-                current->buffer[bufferIndex]->qwords[7] = chgEndUInt64(totalBits);
-                break;
-            default: 
-                current->buffer[bufferIndex]->qwords[7] = totalBits;
-                break;
-        }
+
+#ifdef __x86_64__
+        current->buffer[bufferIndex]->qwords[14] = chgEndUInt64((uint64_t)(totalBits >> 64));
+        current->buffer[bufferIndex]->qwords[15] = chgEndUInt64((uint64_t)(totalBits));
+#endif
+#ifdef _M_ARM64
+        current->buffer[bufferIndex]->qwords[14] = (uint64_t)(totalBits >> 64);
+        current->buffer[bufferIndex]->qwords[15] = (uint64_t)(totalBits);
+#endif
     }
 
     fclose(fp);
@@ -261,11 +228,11 @@ void printBytes()
     current = head;
     for (int i = 0; i < totalNodes; i++)
     {
-        if(i < totalNodes - 1)
+        if (i < totalNodes - 1)
         {
             for (int j = 0; j < NODE_SIZE; j++)
             {
-                for (int k = 0; k < 64; k++)
+                for (int k = 0; k < 128; k++)
                 {
                     printf("\n%02x", current->buffer[j]->bytes[k]);
                 }
@@ -275,7 +242,7 @@ void printBytes()
         {
             for (int j = 0; j < bufferIndex + 1; j++)
             {
-                for (int k = 0; k < 64; k++)
+                for (int k = 0; k < 128; k++)
                 {
                     printf("\n%02x", current->buffer[j]->bytes[k]);
                 }
@@ -292,15 +259,16 @@ void freeBuffers()
     current = head;
     for (int i = 0; i < totalNodes; i++)
     {
-        if (i < totalNodes - 1){        
-            for(int j = 0; j < NODE_SIZE; j++)
+        if (i < totalNodes - 1)
+        {
+            for (int j = 0; j < NODE_SIZE; j++)
             {
                 free(current->buffer[j]);
             }
         }
         else
         {
-            for(int j = 0; j < bufferIndex + 1; j++)
+            for (int j = 0; j < bufferIndex + 1; j++)
             {
                 free(current->buffer[j]);
             }
@@ -309,16 +277,15 @@ void freeBuffers()
     }
 }
 
-void sha256()
+void sha512()
 {
-    uint32_t H[8] = 
-    {
-        0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 
-        0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19
-    };
+    uint64_t H[8] =
+        {
+            0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1,
+            0x510e527fade682d1, 0x9b05688c2b3e6c1f, 0x1f83d9abfb41bd6b, 0x5be0cd19137e2179};
 
-    uint32_t a, b, c, d, e, f, g, h, T1, T2;
-    uint32_t W[64];
+    uint64_t a, b, c, d, e, f, g, h, T1, T2;
+    uint64_t W[80];
 
     int si = NODE_SIZE;
 
@@ -336,18 +303,15 @@ void sha256()
         {
             for (int i = 0; i < 16; i++)
             {
-                switch (littleEndian)
-                {
-                    case true:
-                        W[i] = chgEndUInt32(current->buffer[bu]->dwords[i]);
-                        break;
-                    default:
-                        W[i] = current->buffer[bu]->dwords[i];
-                        break;
-                }
+#ifdef __x86_64__
+                W[i] = chgEndUInt64(current->buffer[bu]->qwords[i]);
+#endif
+#ifdef _M_ARM64
+                W[i] = current->buffer[bu]->qwords[i];
+#endif
             }
 
-            for (int i = 16; i < 64; i++)
+            for (int i = 16; i < 80; i++)
             {
                 W[i] = sig1(W[i - 2]) + W[i - 7] + sig0(W[i - 15]) + W[i - 16];
             }
@@ -361,7 +325,7 @@ void sha256()
             g = H[6];
             h = H[7];
 
-            for (int i = 0; i < 64; i++)
+            for (int i = 0; i < 80; i++)
             {
                 T1 = h + Sig1(e) + ch(e, f, g) + K[i] + W[i];
                 T2 = Sig0(a) + maj(a, b, c);
@@ -393,7 +357,7 @@ void sha256()
 
     for (int i = 0; i < 8; i++)
     {
-        printf("%08llx", H[i]);
+        printf("%016llx", H[i]);
     }
 
     return;
@@ -401,16 +365,15 @@ void sha256()
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2){
+    if (argc != 2)
+    {
         printf("Invalid number of arguments.\nPlease enter a file path.\n");
         exit(0);
     }
 
-    littleEndian = isLittleEndian();
-
     buffGen(argv[1]);
 
-    sha256();
+    sha512();
 
     // printBytes();
     // freeBuffers();
